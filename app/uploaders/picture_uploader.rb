802f2e4307a18735fccd1a06c61456b500ce72d3
Dorothy Eves <dorothyeves7@gmail.com>
2017-09-07 00:07:56 +0000

    add photo to index page and css for styling

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -22,8 +22,8 @@ class PictureUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
-  # Process files as they are uploaded:
-  # process scale: [200, 300]
+   #Process files as they are uploaded:
+   #process scale: [200, 300]
   #
   # def scale(width, height)
   #   # do something
@@ -31,8 +31,8 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  #  process resize_to_fit: [50, 50]
+  #  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
